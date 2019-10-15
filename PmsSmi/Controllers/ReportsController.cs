af3130dcf5246a599822e552fec51ba10f9efdbf
Serhii Michkovskyi <michkovsky@gmail.com>
2019-10-15 17:19:10 +0300

    add xslx formatter stub to pipeline

---
+++ PmsSmi/Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PmsSmi.Data;
+using model = PmsSmi.Data.Model;
+
+namespace PmsSmi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly PmsDbContext _context;
+
+        public ReportsController(PmsDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Projects
+        [HttpGet]
+        public ActionResult<IEnumerable<model.WorkflowItem>> GetReports()
+        {
+            var projects = _context.Projects.ToList();
+            var tasks = _context.Tasks.ToList();
+            var ret = ((from p in projects select p as model.WorkflowItem).Concat(from t in tasks select t as model.WorkflowItem).ToList());
+            return ret;
+        }
+
+    }
+}
--- PmsSmi/Startup.cs
+++ PmsSmi/Startup.cs
@@ -27,7 +27,9 @@ namespace PmsSmi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddControllers(options=> {
+                options.OutputFormatters.Add(new Controllers.XslxFormatter());
+            }).AddNewtonsoftJson();
             services.AddDbContext<PmsDbContext>(options =>
             {
                 options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
