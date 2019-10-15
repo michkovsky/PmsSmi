79c2d9cd52de27f018b6b98bfc1920af024fb7e7
Serhii Michkovskyi <michkovsky@gmail.com>
2019-10-15 11:15:20 +0300

    add db model

---
+++ PmsSmi/Data/Model/WorkflowItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PmsSmi.Data.Model
+{
+    public abstract class WorkflowItem
+    {
+        public int Id { get; set; }
+        public virtual WorkflowItem Parent { get; set; }
+        public string Name { get; set; }
+        public HashSet<WorkflowItem> Childs { get; set; } = new HashSet<WorkflowItem>();
+        public DateTime? StartDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+        public virtual WorkflowState State { get; set; }
+        public virtual WorkflowItemType ItemType { get; set; }
+
+    }
+
+    public class Project : WorkflowItem
+    {
+        public string Code { get; set; }
+        public override WorkflowItem Parent { get => base.Parent; set => base.Parent = value as Project; }
+    }
+
+    public class Task : WorkflowItem
+    {
+        public string Description { get; set; }
+    }
+
+    public enum WorkflowState
+    {
+        Planned = 0,
+        InProgress = 1,
+        Completed = 2,
+    }
+    public enum WorkflowItemType
+    {
+        None,//just oldschool
+        WorkflowItem = None, //it abstact
+        Project,
+        Task,
+    }
+
+
+}
--- PmsSmi/Data/PmsDbContext.cs
+++ PmsSmi/Data/PmsDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PmsSmi.Data.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using model = PmsSmi.Data.Model;
 
 namespace PmsSmi.Data
 {
     public class PmsDbContext:DbContext
     {
         public PmsDbContext(DbContextOptions<PmsDbContext> options) : base(options) { }
+
+        public DbSet<model.Project> Projects { get; set; }
+        public DbSet<model.Task> Tasks { get; set; }
     }
 }
